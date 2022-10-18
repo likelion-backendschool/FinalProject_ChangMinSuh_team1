8651bad25a3a5eff3bf3ba86cc4f4c15f9d239e0
ChangMinSuh <timssuh@naver.com>
2022-10-18 12:53:55 +0900

    feat : SecurityConfig 추가

---
+++ 1Week_Mission/src/main/java/com/example/ebook/auth/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.example.ebook.auth.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http
+                .csrf(
+                        csrf -> csrf.disable()
+                )
+                .authorizeRequests(
+                        authorizeRequests -> authorizeRequests
+                                .antMatchers("/**")
+                                .permitAll()
+                );
+        return http.build();
+    }
+    
+}
